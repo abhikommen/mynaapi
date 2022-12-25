e6d21860b7ad78159566839cf94f4a1a7a988165
Abhishek Dhyani <abhishek.dhyani@IM5430-X0.local>
2022-12-25 19:55:11 +0530

    New api added

--- app.py
+++ app.py
@@ -1,33 +1,39 @@
-from flask import Flask
-from flask import request
-
-import twint
-
-from apis.search import searchUserName
+from flask import Flask, request, Response
+import json
+from apis.tweetscrapper import getTweet, getProfile
 
 app = Flask(__name__)
 
-tweets = []
-
 @app.route('/')
 def index():
-    return "Hello"
+    return "Twitter APi"
     
 
-@app.route('/search')
-def search():
-    # here we want to get the value of user (i.e. ?user=some-value)
+
+@app.route('/tweet')
+def tweet():
     user = request.args.get('user')
     count = request.args.get('count')
-    print(count)
-    jsonResult = searchUserName(user, count)
+    tweetList = getTweet(user, count)
+    jsonArray = parseResponse(tweetList)
+    return jsonArray
+
+
+@app.route('/profile')
+def profile():
+    user = request.args.get('user')
+    profile = getProfile(user)
+    jsonArray = parseResponse(profile)
+    return jsonArray
+
+
+
+def parseResponse(tweetList):
+    jsonResult = []
+    for tweet in tweetList:
+        jsonResult.append(tweet.__dict__)
     return jsonResult
 
 
-def hello():
-    c = twint.Config()
-    c.Username = "elon musk"
-    c.Limit = 10
-    c.Store_object = True
-    c.Store_object_tweets_list = tweets
-    twint.run.Search(c)
+    
+app.run(debug=True, host = "localhost", port = 5555)
