b18982121a1919487f3e4c1c513c2f7f6b7eb075
Abhishek Dhyani <abhishek.dhyani@IM5430-X0.local>
2022-12-26 02:39:08 +0530

    commt

--- server.py
+++ server.py
@@ -1,11 +1,11 @@
 from flask import Flask,render_template
 
-app = Flask('__name__')
+app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return "Hello"
+    return "Hello APi"
 
 @app.route('/endpoint')
 def endpoint():
@@ -15,6 +15,6 @@ def endpoint():
 def linker():
     return render_template('link.html')
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=5000)
+
+app.run(debug = True, host="localhost",port=5555)
     #app.run(debug=True) #can alter host and port number here. Right now the default host is localhost and port is 5000
